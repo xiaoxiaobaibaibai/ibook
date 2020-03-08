5c714ce87443974723cac254b7358f5e5b8906c9
bai <2516220191@qq.com>
2020-03-08 15:52:34 +0800

    A book reading demo

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,20 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="read"></div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import Epub from 'epubjs'
+global.ePub = Epub
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  mounted () {
+    this.book = new Epub('/六朝清羽记&六朝云龙吟.epub')
+    this.book.renderTo('read', {
+      width: window.innerWidth,
+      height: window.innerHeight
+    }).display()
   }
 }
 </script>
---
+++ vue.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    publicPath: process.env.NODE_ENV === 'production' 
+    ? './'
+    : '/'
+        // eslint-disable-next-line eol-last
+}
