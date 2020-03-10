c942dd760bc4efa3486014e02e3c1487f529af6e
bai <2516220191@qq.com>
2020-03-10 23:32:37 +0800

    Slide to achieve page turning

--- src/components/ebook/EbookReader.vue
+++ src/components/ebook/EbookReader.vue
@@ -14,6 +14,17 @@ export default {
         ...mapGetters(['fileName'])
     },
     methods: {
+        prevPage() {
+            if (this.rendition) {
+                this.rendition.prev()
+            }
+        },
+        nextPage() {
+            if (this.rendition) {
+                this.rendition.next()
+            }
+        },
+        toggleTitleAndMenu() {},
         initEpub() {
             const url = 'http://localhost:8081/epub/' + this.fileName + '.epub'
             this.book = new Epub(url)
@@ -24,6 +35,25 @@ export default {
                 methods: 'default' // 微信的兼容性配置
             })
             this.rendition.display()
+            this.rendition.on('touchstart', event => {
+                this.touchStartX = event.changedTouches[0].clientX
+                this.touchStartTime = event.timeStamp
+                //console.log(event)
+            })
+            this.rendition.on('touchend', event => {
+                const offSetX = event.changedTouches[0].clientX - this.touchStartX
+                const time = event.timeStamp - this.touchStartTime
+                // console.log(offSetX, time)
+                if (time < 500 && offSetX > 40) {
+                    this.prevPage()
+                } else if (time < 500 && offSetX < -40) {
+                    this.nextPage()
+                } else {
+                    this.toggleTitleAndMenu()
+                }
+                // event.preventDefault()
+                event.stopPropagation()
+            })
         }
     },
     mounted() {
