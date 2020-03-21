3ae1e093ab38be5508b265a022d7a2a3f0edd8e0
bai <2516220191@qq.com>
2020-03-21 18:47:18 +0800

    Language versionin

--- src/lang/index.js
+++ src/lang/index.js
@@ -1,16 +1,21 @@
 import Vue from 'vue'
-import VueI18n from 'vue-i18n'
+import VueI18N from 'vue-i18n'
 import en from './en'
 import cn from './cn'
+import { getLocale, saveLocale } from '../utils/localStorage'
 
-Vue.use(VueI18n)
+Vue.use(VueI18N)
 
 const messages = {
    en, cn
 }
 
-const locale = 'en'
-const i18n = new VueI18n({
+let locale = getLocale()
+if (!locale) {
+    locale = 'en'
+    saveLocale(locale)
+}
+const i18n = new VueI18N({
     locale,
     messages
 })
