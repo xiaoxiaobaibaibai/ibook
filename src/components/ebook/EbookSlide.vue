4253362e8b793933fcdaba347a6d1ac394938bf0
bai <2516220191@qq.com>
2020-04-05 00:03:53 +0800

    目录载入loading

--- src/components/ebook/EbookSlide.vue
+++ src/components/ebook/EbookSlide.vue
@@ -3,7 +3,7 @@
     <div class="slide-content-wrapper" v-show="menuVisible && settingVisible === 3">
         <transition name="slide-right">
         <div class="content" v-if="settingVisible === 3">
-          <div class="content-page-wrapper">
+          <div class="content-page-wrapper" v-if="bookAvailable">
               <div class="content-page">
                   <component :is="currentTab === 1 ? content : bookmark"></component>
               </div>
@@ -18,6 +18,9 @@
                        {{$t('book.bookmark')}}</div>
               </div>
           </div>
+          <div class="content-empty" v-else>
+              <ebook-loading></ebook-loading>
+          </div>
       </div>
         </transition>
       <div class="content-bg" @click="hideTitleAndMenu()"></div>
@@ -27,8 +30,12 @@
 <script>
 import { ebookMixin } from '../../utils/mixin'
 import EbookSlideContents from './EbookSlideContents'
+import EbookLoading from './EbookLoading'
 export default {
     mixins: [ebookMixin],
+    components: {
+        EbookLoading
+    },
     data() {
         return {
            currentTab: 1,
@@ -78,7 +85,12 @@ export default {
                        @include center;
                    }
                }
-           }           
+           }
+           .content-empty {
+               width: 100%;
+               height: 100%;
+               @include center;
+           }
        }
        .content-bg {
            flex: 0 0 15%;
